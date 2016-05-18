053bf20cc99d0c39971af40812687ac4be20965c
RubenKelevra <ruben@vfn-nrw.de>
2016-05-18 08:58:48 +0200

    fix some stuff

--- init_backupstore.sh
+++ init_backupstore.sh
@@ -1,21 +1,22 @@
-DEVICE="/dev/sdx"
+DEVICE="/dev/sdX"
 ROOT_FOLDER="/backupstore"
 
 
 sudo cryptsetup -v --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 5000 --use-random luksFormat "$DEVICE"
 sudo cryptsetup luksOpen "$DEVICE" backup
 sudo mkdir -p "$ROOT_FOLDER"
-sudo zpool create -f -o ashift=12 -m "$ROOT_FOLDER" backupstore $DEVICE
+sudo zpool create -f -m "$ROOT_FOLDER" backupstore /dev/mapper/backup
 sudo zfs create backupstore/root
 
-sudo zfs set relatime=on        backupstore/root
+sudo zfs set atime=off        backupstore/root
 sudo zfs set compression=gzip-9 backupstore/root
 sudo zfs set dedup=on           backupstore/root
+sudo zfs set redundant_metadata=most backupstore/root
 
 sudo zfs set mountpoint="$ROOT_FOLDER" backupstore
 
 sudo zfs umount /backupstore || true
-sudo zfs umount -f /backupstore || true
+sudo zfs export backupstore || true
 sudo cryptsetup luksClose backup 
 sudo eject $DEVICE
 
